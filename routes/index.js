627ea002ae1a35d5fddfe17244b0d244c5528bd2
Ryan Boswell <ryan@ryanboswell.com>
2015-06-16 21:03:48 -0700

    Whalespin: Now with more whales, at random!

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,19 @@ var router            = express.Router();
  * Homepage
  */
 router.get('/', function(req, res) {
-  res.render('index', { title: "whalespin.co" });
+
+  // Array of whale gifs
+  var whales = [
+    '/images/whales/trainer.gif',
+    '/images/whales/trainer-reverse.gif',
+    '/images/whales/no-regrets.gif'
+  ];
+
+  // Pick an image at random
+  var index = Math.floor(Math.random() * whales.length);
+
+  // Pass the random GIF url to the template engine
+  res.render('index', { title: "whalespin.co", whale: whales[index] });
 });
 
 module.exports = router;
